7423a46336dd661cd4562f39e0d7474f4aa2feb0
Alyssa Kim <alyssa.kim@wustl.edu>
2024-11-13 11:59:17 -0600

    final commit with fully working tic tac toe board

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import { useState } from 'react';
 
-function Square({value, onSquareClick}) {
+function Square({ value, onSquareClick }) {
   return (
     <button className="square" onClick={onSquareClick}>
       {value}
